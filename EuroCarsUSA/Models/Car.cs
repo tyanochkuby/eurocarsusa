f95a92c75dc218924d494d306196e34a98ef08e5
tyanochku_by <ramyud@st.amu.edu.pl>
2024-04-17 13:08:45 +0200

    init

---
+++ EuroCarsUSA/Models/Car.cs
@@ -0,0 +1,12 @@
+﻿namespace EuroCarsUSA.Models
+{
+    public class Car
+    {
+        public int Id { get; set; }
+        public string Make { get; set; }
+        public string Color { get; set; }
+        public string VIN { get; set; }
+        public Car() { }
+
+    }
+}
