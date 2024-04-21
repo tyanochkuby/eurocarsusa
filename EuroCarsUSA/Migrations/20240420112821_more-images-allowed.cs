ccf42fd114ff941fd5187bc4920926d050a31b7a
tyanochku_by <ramyud@st.amu.edu.pl>
2024-04-21 08:22:31 +0200

    migration and detail page view

---
+++ EuroCarsUSA/Migrations/20240420112821_more-images-allowed.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EuroCarsUSA.Migrations
+{
+    /// <inheritdoc />
+    public partial class moreimagesallowed : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Image",
+                table: "Cars",
+                newName: "Images");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Images",
+                table: "Cars",
+                newName: "Image");
+        }
+    }
+}
