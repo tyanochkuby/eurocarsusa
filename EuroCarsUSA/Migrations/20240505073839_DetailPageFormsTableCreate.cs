eb37a782e3863e777fe1025244337641d7cb2503
countingthedots <countingthedots@gmail.com>
2024-05-05 09:42:01 +0200

    ECU-35 DetailPageForm model and migration created

---
+++ EuroCarsUSA/Migrations/20240505073839_DetailPageFormsTableCreate.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EuroCarsUSA.Migrations
+{
+    /// <inheritdoc />
+    public partial class DetailPageFormsTableCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "DetailPageForms",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(40)", maxLength: 40, nullable: true),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    PhoneNumber = table.Column<string>(type: "nvarchar(9)", maxLength: 9, nullable: true),
+                    CarId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Message = table.Column<string>(type: "nvarchar(200)", maxLength: 200, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_DetailPageForms", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_DetailPageForms_Cars_CarId",
+                        column: x => x.CarId,
+                        principalTable: "Cars",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_DetailPageForms_CarId",
+                table: "DetailPageForms",
+                column: "CarId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "DetailPageForms");
+        }
+    }
+}
---
+++ EuroCarsUSA/Models/DetailPageForm.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace EuroCarsUSA.Models
+{
+    public class DetailPageForm
+    {
+        [Key]
+        public Guid Id { get; set; }
+
+        [MaxLength(40)]
+        public string? Name { get; set; }
+
+        [EmailAddress]
+        public string? Email { get; set; }
+
+        [Phone]
+        [MaxLength(9)]
+        public string? PhoneNumber { get; set; }
+
+        [ForeignKey("Car")]
+        public Guid CarId { get; set; }
+        public Car Car { get; set; }
+
+        [MaxLength(200)]
+        public string? Message { get; set; }
+
+        public DetailPageForm() { }
+    }
+}
