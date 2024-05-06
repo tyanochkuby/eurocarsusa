ff33e73339b6940be9c2838b8e5b2dba953ccd2c
tyanochku_by <ramyud@st.amu.edu.pl>
2024-05-06 19:14:49 +0200

    ECU-37 new view index.cshtml with grid added, new partial view GetMoreCars.cshtml added, show more button works, more cars added to Seed.cs

--- EuroCarsUSA/Data/Enum/CarType.cs
+++ EuroCarsUSA/Data/Enum/CarType.cs
@@ -12,5 +12,6 @@
         Pickup,
         Van,
         Wagon,
+        Sports
     }
 }
--- EuroCarsUSA/Data/Seed.cs
+++ EuroCarsUSA/Data/Seed.cs
@@ -17,7 +17,7 @@ namespace EuroCarsUSA.Data
 
                 if (true)//!context.Cars.Any())
                 {
-                    context.Database.ExecuteSqlRaw("TRUNCATE TABLE Cars");
+                    //context.Database.ExecuteSqlRaw("TRUNCATE TABLE Cars");
                     context.Cars.AddRange(new List<Car>()
                     {
                         new Car()
@@ -27,16 +27,17 @@ namespace EuroCarsUSA.Data
                             Model = "Model S P100D",
                             Color = "Red",
                             VIN = "12345364758",
-                            Images = new List<string>{ 
+                            Images = new List<string> {
                                 "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS2EqfTpmjeC6G23-aYZFOV55RfNpZ7rdRj7FQkzqCXjQ&s",
                                 "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQGIRxwSRMqNNnahDvBfT3dsBfTC-nwAtph5DfNtXONaA&s",
                                 "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS8noXZd9o7-nHxx4jZjG7NEcdbSt-Xn_8PxvKTSjr3yA&s",
                                 "https://i.pinimg.com/736x/02/f9/eb/02f9ebc30503b86c7bbbc7c20915a5d2.jpg"
                             },
                             FuelType = Enum.CarFuelType.Electric,
-                            Year=2018,
+                            Year = 2018,
                             Mileage = 67890,
                             Price = 350_000,
+                            Transmission = CarTransmission.Automatic,
                         },
                         new Car()
                         {
@@ -45,7 +46,7 @@ namespace EuroCarsUSA.Data
                             Model = "Countryman",
                             Color = "Blue",
                             VIN = "8765432",
-                            Images = new List<string>{ "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTDMkmd2hpZbC6_PeQRW9lViM3Aq17Bj2SU92A8FMOTyA&s" },
+                            Images = new List<string> { "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTDMkmd2hpZbC6_PeQRW9lViM3Aq17Bj2SU92A8FMOTyA&s" },
                             FuelType = Enum.CarFuelType.Benzine,
                             Year = 2020,
                             Mileage = 16734,
@@ -90,7 +91,7 @@ namespace EuroCarsUSA.Data
                             },
                             FuelType = CarFuelType.Benzine,
                             Year = 2021,
-                            Mileage= 31493,
+                            Mileage = 31493,
                             EngineVolume = 3500,
                             Transmission = CarTransmission.Manual,
                             Price = 220_000,
@@ -114,6 +115,7 @@ namespace EuroCarsUSA.Data
                             Year = 2022,
                             Mileage = 3242,
                             Price = 200_000,
+                            Transmission = CarTransmission.Automatic,
                         },
                         new Car()
                         {
@@ -136,10 +138,118 @@ namespace EuroCarsUSA.Data
                             EngineVolume = 3200,
                             Transmission = CarTransmission.Automatic,
                             Price = 400_000,
+                        },
+                        new Car()
+                        {
+                            Make = CarMake.Audi,
+                            Type = CarType.Sedan,
+                            Model = "A8",
+                            Color = "White",
+                            VIN = "123456789",
+                            Images = new List<string>
+                            {
+                                "https://media.carsandbids.com/cdn-cgi/image/width=2080,quality=70/d9b636c2ec84ddc3bc7f2eb32861b39bdd5f9683/photos/rbAPeDR2.4MFH1uv1J-(edit).jpg?t=162843319241",
+                            },
+                            FuelType = CarFuelType.Benzine,
+                            Year = 2020,
+                            Mileage = 10000,
+                            EngineVolume = 3000,
+                            Transmission = CarTransmission.Automatic,
+                            Price = 500_000,
+                        },
+                        new Car()
+                        {
+                            Make = CarMake.MercedesBenz,
+                            Type = CarType.SUV,
+                            Model = "GLC",
+                            Color = "Black",
+                            VIN = "987654321",
+                            Images = new List<string>
+                            {
+                                "https://inv.assets.ansira.net/ChromeColorMatch/us/WHITE_cc_2024MBS680002_01_1280_040.jpg",
+                            },
+                            FuelType = CarFuelType.Benzine,
+                            Year = 2021,
+                            Mileage = 5000,
+                            EngineVolume = 3500,
+                            Transmission = CarTransmission.Automatic,
+                            Price = 600_000,
+                        },
+                        new Car()
+                        {
+                            Make = CarMake.BMW,
+                            Type = CarType.Sedan,
+                            Model = "7 Series",
+                            Color = "Blue",
+                            VIN = "246813579",
+                            Images = new List<string>
+                            {
+                                "https://cdn.motor1.com/images/mgl/9mmmOl/s3/2023-bmw-7-series-the-first-edition-for-japan.jpg",
+                            },
+                            FuelType = CarFuelType.Benzine,
+                            Year = 2022,
+                            Mileage = 2000,
+                            EngineVolume = 4000,
+                            Transmission = CarTransmission.Automatic,
+                            Price = 700_000,
+                        },
+                        new Car()
+                        {
+                            Make = CarMake.Tesla,
+                            Type = CarType.Sedan,
+                            Model = "Model X",
+                            Color = "Red",
+                            VIN = "135792468",
+                            Images = new List<string>
+                            {
+                                "https://issimi-vehicles-cdn.b-cdn.net/publicamlvehiclemanagement/VehicleDetails/109/1.jpg?width=3840&quality=75",
+                            },
+                            FuelType = CarFuelType.Electric,
+                            Year = 2023,
+                            Mileage = 1000,
+                            Price = 800_000,
+                            Transmission = CarTransmission.Automatic,
+                        },
+                        new Car()
+                        {
+                            Make = CarMake.Porsche,
+                            Type = CarType.Sports,
+                            Model = "911",
+                            Color = "Yellow",
+                            VIN = "864209753",
+                            Images = new List<string>
+                            {
+                                "https://www.austinclearbra.com/wp-content/uploads/sites/6/2021/09/2018-Porsche-911-GT3-1-scaled.jpg",
+                            },
+                            FuelType = CarFuelType.Benzine,
+                            Year = 2024,
+                            Mileage = 500,
+                            EngineVolume = 4500,
+                            Transmission = CarTransmission.Manual,
+                            Price = 900_000,
+                        },
+                        new Car()
+                        {
+                            Make = CarMake.Lamborghini,
+                            Type = CarType.Sports,
+                            Model = "Aventador",
+                            Color = "Green",
+                            VIN = "102938475",
+                            Images = new List<string>
+                            {
+                                "https://media.gq-magazine.co.uk/photos/5d13ad1aeef92171f5a0072b/16:9/w_2560%2Cc_limit/aventador-04-gq-24aug18_b.jpg",
+                            },
+                            FuelType = CarFuelType.Benzine,
+                            Year = 2020,
+                            Mileage = 13244,
+                            EngineVolume = 4500,
+                            Transmission = CarTransmission.Automatic,
+                            Price = 1_000_000,
+
                         }
                     });
+                    context.SaveChanges();
                 }
-                context.SaveChanges();
             }
         }
     }
