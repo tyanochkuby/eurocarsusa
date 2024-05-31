7a1c7207bdbc46073f3d6508b4145a2a481ff97c
tyanochku_by <ramyud@st.amu.edu.pl>
2024-05-31 14:04:40 +0200

    ECU-78 CarColor enum added

---
+++ EuroCarsUSA/Data/Enum/CarColor.cs
@@ -0,0 +1,19 @@
+﻿namespace EuroCarsUSA.Data.Enum
+{
+    public enum CarColor
+    {
+        Blue,
+        Red,
+        Green,
+        Yellow,
+        Black,
+        White,
+        Silver,
+        Gray,
+        Brown,
+        Orange,
+        Purple,
+        Pink,
+        Beige
+    }
+}
