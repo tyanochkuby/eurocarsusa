d2bee94f2ca2670c218ff8e6e64d5030b13b5940
galezzza <egor.galeznik.87@gmail.com>
2024-11-06 20:48:30 +0100

    Created button and icon struture

---
+++ EuroCarsUSA/Extensions/CustomViewLocationExpander.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc.Razor;
+using System.Collections.Generic;
+
+public class CustomViewLocationExpander : IViewLocationExpander
+{
+    public void PopulateValues(ViewLocationExpanderContext context)
+    {
+    }
+
+    public IEnumerable<string> ExpandViewLocations(ViewLocationExpanderContext context, IEnumerable<string> viewLocations)
+    {
+        var customLocations = new[]
+        {
+            "/Views/Shared/Components/{0}.cshtml"
+        };
+
+        return customLocations.Concat(viewLocations);
+    }
+}
