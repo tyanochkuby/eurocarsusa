641f09744be8fd90d677786a9685a43122fdd78c
tyanochku_by <romyud1994@gmail.com>
2025-01-03 23:06:39 +0100

    ECU-210 add track order functionality; fix show more button

---
+++ EuroCarsUSA/Migrations/20250103220038_FixGerCarsCount.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EuroCarsUSA.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixGerCarsCount : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            var basePath = Path.Combine(AppContext.BaseDirectory, "Migrations", "Procedures");
+
+            // Execute GetFilteredCars.sql
+            var getFilteredCarsCountPath = Path.Combine(basePath, "GetFilteredCarsCount1.sql");
+            if (File.Exists(getFilteredCarsCountPath))
+            {
+                var getFilteredCarsSql = File.ReadAllText(getFilteredCarsCountPath);
+                migrationBuilder.Sql(getFilteredCarsSql);
+            }
+            else
+            {
+                throw new FileNotFoundException($"SQL file '{getFilteredCarsCountPath}' not found.");
+            }
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            var basePath = Path.Combine(AppContext.BaseDirectory, "Migrations", "Procedures");
+            var getFilteredCarsCountPath = Path.Combine(basePath, "GetFilteredCarsCount.sql");
+            if (File.Exists(getFilteredCarsCountPath))
+            {
+                var getFilteredCarsCountSql = File.ReadAllText(getFilteredCarsCountPath);
+                migrationBuilder.Sql(getFilteredCarsCountSql);
+            }
+            else
+            {
+                throw new FileNotFoundException($"SQL file '{getFilteredCarsCountPath}' not found.");
+            }
+        }
+    }
+}
