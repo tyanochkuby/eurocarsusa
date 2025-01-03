9b759d246398789220a8be23859d474c817fcb07
galezzza <egor.galeznik.87@gmail.com>
2025-01-03 22:16:27 +0100

    Edit Catalog chaged components design

--- EuroCarsUSA/Views/Shared/Components/_EditableFIeld.cshtml.cs
+++ EuroCarsUSA/Views/Shared/Components/_EditableFIeld.cshtml.cs
@@ -8,5 +8,7 @@ public class EditableField
     public Enum SelectedValue { get; set; }
     public string CssClass { get; set; } = "";
     public string Style { get; set; } = "";
+
+    public string Overline { get; set; } = "Overline";
     public string Name { get; set; }
 }
